c4d35708a33b15dfdfd8342beec74313253d25ba
kkgaiils <student@C426U26>
2020-09-08 14:08:45 +0300

    finish1

--- PR1/01/Program.cs
+++ PR1/01/Program.cs
@@ -6,11 +6,26 @@ namespace pr1_z1
   {
     struct Person
     {
+      public string firstName;
+      public string lastName;
+      public int age;
+      public Person(string _firstName, string _lastName, int _age)
+      {
+        firstName = _firstName;
+        lastName = _lastName; age = _age;
+      }
+      public override string ToString()
+      {
+        return firstName + " " + lastName + ", age " + age;
+      }
+
     }
 
     static void Main(string[] args)
+    
     {
       Console.WriteLine("Hello World!");
+      Person p = new Person("Tony", "Allen", 32); Console.WriteLine(p);
     }
   }
 }
