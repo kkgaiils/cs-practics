00e5ff6d25bf533f8dbbd0460645cfc5ca5f8194
kkgaiils <student@C426U26>
2020-09-08 13:57:48 +0300

    start project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pr1_z1
+{
+  class Program
+  {
+    struct Person
+    {
+    }
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
